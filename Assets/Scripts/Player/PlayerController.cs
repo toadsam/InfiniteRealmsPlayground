10ba2f4c3643fff289adc2070f3e7ef584a211bb
toadsam <toadsam@naver.com>
2024-02-02 23:21:02 +0900

    [ADD] 플레이어 이동속도 제어 (-재훈)
    플레이어 이동속도 제어

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    protected Animator animator;
+    protected const float DEFAULT_CONVERT_MOVESPEED = 3f;
+    protected const float DEFAULT_ANIMATION_PLAYSPEED = 0.9f;
     protected Player player;
     public Vector3 direction { get; private set; }
 
@@ -16,6 +19,7 @@ public class PlayerController : MonoBehaviour
     protected const float CONVERT_UNIT_VALUE = 0.01f;
     void Start()
     {
+        animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
         player = GetComponent<Player>();
         //rigidbody = GetComponent<Rigidbody>();
     }
@@ -24,17 +28,29 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 input = context.ReadValue<Vector2>();
         direction = new Vector3(input.x, 0f, input.y);
-        Debug.Log("열어봐");
+        //Debug.Log("열어봐");
     }
 
     protected void Move()
     {
         float currentMoveSpeed = player.MoveSpeed * CONVERT_UNIT_VALUE;
+        float animationPlaySpeed = DEFAULT_ANIMATION_PLAYSPEED + GetAnimationSyncWithMovement(currentMoveSpeed);
         LookAt();
         rigidbody.velocity = direction * currentMoveSpeed + Vector3.up * rigidbody.velocity.y;
-         //Debug.Log(direction);
+        animator.SetFloat("Velocity" , animationPlaySpeed);
+        //Debug.Log(direction);
         // Debug.Log(Vector3.up);
         // Debug.Log(rigidbody.velocity.y);
+    }
+
+    protected float GetAnimationSyncWithMovement(float changedMoveSpeed)
+    {
+        if (direction == Vector3.zero)
+        {
+            return -DEFAULT_ANIMATION_PLAYSPEED;
+        }
+
+        return (changedMoveSpeed - DEFAULT_CONVERT_MOVESPEED) * 0.1f;
     }
 
     protected void LookAt()
